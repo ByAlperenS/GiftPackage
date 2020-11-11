509ce4451a934ce1c5c8f8d08d27cc33f930a00b
Alperen Sancak <49764317+ByAlperenS@users.noreply.github.com>
2020-11-11 21:58:19 +0300

    Add files via upload

---
+++ src/GiftPackage/Entity/GiftPackageEntity.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * Author: ByAlperenS
+ * Contect:
+ *   - Messenger: Alperen Sancak
+ *   - Facebook: Alperen Sancak
+ *   - Discord: ByAlperenS#5361
+ *
+ * Turkey - 2020
+ *
+ */
+
+namespace GiftPackage\Entity;
+
+use GiftPackage\GiftPackage;
+use pocketmine\entity\Human;
+use pocketmine\entity\Skin;
+
+class GiftPackageEntity extends Human{
+
+    public $height = 1.8;
+    public $width = 0.6;
+
+    public function setSkin(Skin $skin) : void{
+        parent::setSkin(new Skin($skin->getSkinId(), $skin->getSkinData(), '', 'geometry.giftpackage', file_get_contents(GiftPackage::getInstance()->getDataFolder() . "giftpackage.json")));
+    }
+}
