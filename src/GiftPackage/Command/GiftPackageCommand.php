d2dd380e6e44238917f08e3cbd3f94dd2d1fea7d
Alperen Sancak <49764317+ByAlperenS@users.noreply.github.com>
2020-11-13 20:10:51 +0300

    Add files via upload

---
+++ src/GiftPackage/Command/GiftPackageCommand.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * Author: ByAlperenS
+ * Contact:
+ *   - Messenger: Alperen Sancak
+ *   - Facebook: Alperen Sancak
+ *   - Discord: ByAlperenS#5361
+ *
+ * Turkey - 2020
+ *
+ */
+
+namespace GiftPackage\Command;
+
+use GiftPackage\GiftPackage;
+use pocketmine\command\{PluginCommand, CommandSender};
+use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as C;
+
+class GiftPackageCommand extends PluginCommand{
+
+    /** @var GiftPackage */
+    private $plugin;
+
+    public function __construct(GiftPackage $plugin){
+        parent::__construct("giftpackage", $plugin);
+        $this->setAliases(['gp']);
+        $this->setDescription("Gift Package Commands");
+        $this->setUsage("/gp help");
+        $this->plugin = $plugin;
+    }
+
+    public function execute(CommandSender $p, string $commandLabel, array $args){
+        $config = $this->plugin->configGet();
+        $plugintitle = $config->get("Plugin-Title");
+        if (!$p instanceof Player){
+            $p->sendMessage("Please Use This Command In-Game !");
+            return false;
+        }
+        if (isset($args[0])){
+            switch ($args[0]){
+                case "help":
+                    $p->sendMessage(C::GRAY . "[ " . C::RED . "GiftPackage" . C::GRAY . " ]");
+                    $p->sendMessage(C::GRAY . "/giftpackage help");
+                    $p->sendMessage(C::GRAY . "/giftpackage buy [Count]");
+                    break;
+                case "buy":
+                     $cost = $config->get("GiftPackage-Cost");
+                    if (isset($args[1])){
+                        if (!is_numeric($args[1])){
+                            $p->sendMessage(str_replace("{title}", $plugintitle, $config->get("No-Numerical-Message")));
+                            return false;
+                        }
+                        if ($this->plugin->economyGet()->myMoney($p) < $args[1] * $cost){
+                            $p->sendMessage(str_replace("{title}", $plugintitle, $config->get("No-Money-Message")));
+                            return false;
+                        }
+                        $this->plugin->economyGet()->reduceMoney($p, $args[1] * $cost);
+                        $item = ItemFactory::get(Item::CHEST);
+                        $item->setCustomName(C::GOLD . "Gift Package");
+                        $item->setLore(["GiftPackage"]);
+                        $item->setCount($args[1]);
+                        $p->getInventory()->addItem($item);
+                        $p->sendMessage(str_replace("{title}", $plugintitle, $config->get("Buy-Item-Message")));
+                    }else{
+                        $p->sendMessage(C::GRAY . $this->getUsage());
+                    }
+                    break;
+            }
+        }else{
+            $p->sendMessage(C::GRAY . $this->getUsage());
+        }
+        return true;
+    }
+}
---
+++ src/GiftPackage/Utils.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * Author: ByAlperenS
+ * Contact:
+ *   - Messenger: Alperen Sancak
+ *   - Facebook: Alperen Sancak
+ *   - Discord: ByAlperenS#5361
+ *
+ * Turkey - 2020
+ *
+ */
+
+namespace GiftPackage;
+
+use pocketmine\entity\Skin;
+
+class Utils{
+
+    public static function getSkinFromFile(string $path) : ?Skin{
+        $img = @imagecreatefrompng($path);
+        $bytes = '';
+        $l = (int) @getimagesize($path)[1];
+        for($y = 0; $y < $l; $y++){
+            for($x = 0; $x < 64; $x++){
+                $rgba = @imagecolorat($img, $x, $y);
+                $a = ((~((int)($rgba >> 24))) << 1) & 0xff;
+                $r = ($rgba >> 16) & 0xff;
+                $g = ($rgba >> 8) & 0xff;
+                $b = $rgba & 0xff;
+                $bytes .= chr($r) . chr($g) . chr($b) . chr($a);
+            }
+        }
+        @imagedestroy($img);
+        return new Skin("giftpackage", $bytes);
+    }
+}
